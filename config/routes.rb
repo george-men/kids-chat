d28d4da6f25554d06ef573c30d08d55a27de7b12
george-men <jyouoshikiri@gmail.com>
2020-10-01 00:21:36 +0900

    ビューファイル完全格納、マイグレーション全て完了、ユーザー管理機能完全実装、コントローラ全て生成

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   get 'messages/index'
   get 'kids/index'
   root to: "kids#index"
-  resources :users
-  resources :kids do
-    resources :rooms, only: [:new, :create, :destroy] do
-      resources :messages, only: [:index, :create]
-    end
-  end
+  resources :kids 
+  devise_for :users
+  resources :rooms, only: [:new, :create, :destroy] 
+  resources :messages, only: [:index, :create]
 end
